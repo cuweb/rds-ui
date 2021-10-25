5d9ead439bd0848e075c07902ce814417f75bc2f
Marcelo <glacial@gmail.com>
2021-10-25 14:23:12 -0400

    add docs mode

---
+++ .storybook/RdsTheme.js
@@ -0,0 +1,10 @@
+// .storybook/YourTheme.js
+
+import { create } from '@storybook/theming';
+
+export default create({
+  base: 'light',
+  brandTitle: 'Raven Design System',
+  // brandImage:
+  //   'https://carleton.ca/brand/wp-content/uploads/brand-logo-800w-1.jpg',
+});
