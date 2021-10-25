d6f397ded084cceddce8cace4001c28a15c8b08e
Marcelo <glacial@gmail.com>
2021-10-25 12:48:33 -0400

    remove next and fix tailwind

--- components/Button/Button.spec.js
+++ components/Button/Button.spec.js
@@ -4,20 +4,28 @@ describe('Button', () => {
     ghost: `.c-buttoncta--ghost`,
     full: `.c-buttoncta--full`,
   };
-  before(() => {
-    cy.clearCookies();
-    cy.visit(`${Cypress.env('baseUrl')}/`);
-  });
 
   it(`Should render the default button`, () => {
+    cy.clearCookies();
+    cy.visit(
+      `${Cypress.env('baseUrl')}/iframe.html?id=components-button--default`
+    );
     cy.get(button.default).should('exist');
   });
 
   it(`Should render the ghost button`, () => {
+    cy.clearCookies();
+    cy.visit(
+      `${Cypress.env('baseUrl')}/iframe.html?id=components-button--ghost`
+    );
     cy.get(button.ghost).should('exist');
   });
 
   it(`Should render the full width button`, () => {
+    cy.clearCookies();
+    cy.visit(
+      `${Cypress.env('baseUrl')}/iframe.html?id=components-button--full-width`
+    );
     cy.get(button.full).should('exist');
   });
 });
