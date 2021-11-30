d7bcc2e32d5fe653c0b83bb689efa35b740fb8c2
Marcelo <glacial@gmail.com>
2021-11-30 15:22:29 -0500

    update storybook

--- .storybook/main.js
+++ .storybook/main.js
@@ -1,27 +1,22 @@
-const path = require('path')
+const path = require('path');
 
 module.exports = {
-    stories: [
-        '../components/**/*.stories.mdx',
-        '../components/**/*.stories.@(js|jsx|ts|tsx)',
-    ],
-    addons: [
-        '@storybook/addon-links',
-        '@storybook/addon-essentials',
-        {
-            name: '@storybook/addon-postcss',
-            options: {
-                postcssLoaderOptions: {
-                    implementation: require('postcss'),
-                },
-            },
-        },
-    ],
-    webpackFinal: async (config) => {
-        config.resolve.alias = {
-            ...config.resolve.alias,
-            '@components': path.resolve(__dirname, '../components/'),
-        }
-        return config
-    },
-}
+  stories: ['../components/**/*.stories.mdx', '../components/**/*.stories.@(js|jsx|ts|tsx)'],
+  addons: ['@storybook/addon-links', '@storybook/addon-essentials', {
+    name: '@storybook/addon-postcss',
+    options: {
+      postcssLoaderOptions: {
+        implementation: require('postcss')
+      }
+    }
+  }],
+  webpackFinal: async config => {
+    config.resolve.alias = { ...config.resolve.alias,
+      '@components': path.resolve(__dirname, '../components/')
+    };
+    return config;
+  },
+  core: {
+    builder: "webpack5"
+  }
+};
