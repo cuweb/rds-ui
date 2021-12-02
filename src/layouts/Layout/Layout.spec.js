ffa86d59d50b4c12f6cb40be3b82865d24e08b03
Marcelo <glacial@gmail.com>
2021-12-02 09:53:09 -0500

    add more tests to layout

--- src/layouts/Layout/Layout.spec.js
+++ src/layouts/Layout/Layout.spec.js
@@ -19,6 +19,9 @@ describe(title, () => {
         it(`${variation.type.toUpperCase()}: Should render the main tag`, () => {
             cy.get(`.l-multicol--${variation.type} main`).should('exist')
         })
+        it(`${variation.type.toUpperCase()}: Should accept main content`, () => {
+            cy.get(`.l-multicol--${variation.type} main`).should('not.be.empty')
+        })
     })
 
     it(`AM: Should render aside tag`, () => {
@@ -28,6 +31,7 @@ describe(title, () => {
             )}/iframe.html?id=main-concepts-layouts--multicol-am`
         )
         cy.get(`.l-multicol--am aside.multicol__nav`).should('exist')
+        cy.get(`.l-multicol--am aside.multicol__nav`).should('not.be.empty')
     })
 
     it(`MA: Should render aside tag`, () => {
@@ -37,6 +41,7 @@ describe(title, () => {
             )}/iframe.html?id=main-concepts-layouts--multicol-ma`
         )
         cy.get(`.l-multicol--ma aside.multicol__nav`).should('exist')
+        cy.get(`.l-multicol--ma aside.multicol__nav`).should('not.be.empty')
     })
 
     it(`AMA: Should render aside and sidebar tags`, () => {
@@ -46,6 +51,8 @@ describe(title, () => {
             )}/iframe.html?id=main-concepts-layouts--multicol-ama`
         )
         cy.get(`.l-multicol--ama aside.multicol__nav`).should('exist')
+        cy.get(`.l-multicol--ama aside.multicol__nav`).should('not.be.empty')
         cy.get(`.l-multicol--ama aside.multicol__sidebar`).should('exist')
+        cy.get(`.l-multicol--ama aside.multicol__nav`).should('not.be.empty')
     })
 })
