e7beba5d53acdc9f5722d305f4e2446bec5c29fd
Michael Corkum <mcorkum@gmail.com>
2021-12-08 09:23:23 -0500

    Adding HTML tab on canvas

--- .storybook/main.js
+++ .storybook/main.js
@@ -1,9 +1,8 @@
 module.exports = {
-  "stories": [
-    "../src/**/*.stories.tsx"
-  ],
-  "addons": [
-    "@storybook/addon-links",
-    "@storybook/addon-essentials"
-  ]
+    stories: ['../src/**/*.stories.tsx'],
+    addons: [
+        '@storybook/addon-links',
+        '@storybook/addon-essentials',
+        '@whitespace/storybook-addon-html',
+    ],
 }
