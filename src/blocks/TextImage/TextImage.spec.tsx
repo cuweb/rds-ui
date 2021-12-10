0249a81b08e2f2f5ee1e9ee554259d1de281067c
HanqingZhou <72098908+HanqingZhou@users.noreply.github.com>
2021-12-10 16:02:10 -0500

    WSDEV-759-text-image-add more tests

--- src/blocks/TextImage/TextImage.spec.tsx
+++ src/blocks/TextImage/TextImage.spec.tsx
@@ -9,7 +9,7 @@ describe('TextImage', () => {
     ]
 
     types.map((type) => {
-        it(`${type}: Should render the class`, () => {
+        it(`${type}: Should render the text image`, () => {
             cy.visit(
                 `${Cypress.env(
                     'baseUrl'
@@ -18,7 +18,7 @@ describe('TextImage', () => {
             cy.get(`.b-textimg`).should('exist')
         })
 
-        it(`${type}: Should render the wrapper`, () => {
+        it(`${type}: Should render the text image wrapper`, () => {
             cy.visit(
                 `${Cypress.env(
                     'baseUrl'
@@ -27,7 +27,7 @@ describe('TextImage', () => {
             cy.get(`.textimg__wrapper`).should('exist')
         })
 
-        it(`${type}: Should render the image`, () => {
+        it(`${type}: Should render the text image text`, () => {
             cy.visit(
                 `${Cypress.env(
                     'baseUrl'
@@ -58,4 +58,44 @@ describe('TextImage', () => {
         )
         cy.get(`.u-block--grey`).should('exist')
     })
+
+
+    // Default Headings
+
+    it(`Default Headings: Should render the default headings`, () => {
+        cy.visit(
+            `${Cypress.env(
+                'baseUrl'
+            )}/iframe.html?id=blocks-textimage--default-headings`
+        )
+        cy.get(`.c-heading-`).should('exist')
+    })
+
+        // Centered Headings
+
+        it(`Centered Headings: Should render the centered headings`, () => {
+            cy.visit(
+                `${Cypress.env(
+                    'baseUrl'
+                )}/iframe.html?id=blocks-textimage--centered-headings`
+            )
+            cy.get(`.c-heading-`).should('exist')
+            cy.get(`.c-heading--center`).should('exist')
+
+        })
+
+        // Large Width Blocks
+
+        it(`Large Width Blocks: Should render the large width blocks`, () => {
+            cy.visit(
+                `${Cypress.env(
+                    'baseUrl'
+                )}/iframe.html?id=blocks-textimage--large-width-blocks`
+            )
+            cy.get(`.u-block--white`).should('exist')
+
+        })
+
+        
+
 })
