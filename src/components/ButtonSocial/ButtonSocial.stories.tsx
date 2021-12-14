cf0764b5d6a8902f8c8364f4c8fd7e11a0dce690
Marcelo <glacial@gmail.com>
2021-12-14 08:23:13 -0500

    WSDEV-723 - add all the stories

--- src/components/ButtonSocial/ButtonSocial.stories.tsx
+++ src/components/ButtonSocial/ButtonSocial.stories.tsx
@@ -22,3 +22,31 @@ Instagram.args = {
     link: 'https://www.instagram.com/carleton_u',
     text: 'Follow us on Instagram',
 }
+
+export const Twitter = Template.bind({})
+Twitter.args = {
+    type: 'twitter',
+    link: 'https://twitter.com/@Carleton_U',
+    text: 'Follow us on Twitter',
+}
+
+export const Youtube = Template.bind({})
+Youtube.args = {
+    type: 'twitter',
+    link: 'https://www.youtube.com/user/carletonuvideos',
+    text: 'Subscribe to our Youtube channel',
+}
+
+export const Linkedin = Template.bind({})
+Linkedin.args = {
+    type: 'linkedin',
+    link: 'https://www.linkedin.com/school/carleton-university',
+    text: 'View us on Linkedin',
+}
+
+export const Pinterest = Template.bind({})
+Pinterest.args = {
+    type: 'pinterest',
+    link: 'https://www.pinterest.ca/carletonu',
+    text: 'Check us out on Pinterest',
+}
