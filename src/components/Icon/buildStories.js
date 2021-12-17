3e2773c956119e46beb3d7de343735da22034b64
Marcelo <glacial@gmail.com>
2021-12-17 08:40:32 -0500

    WSDEV-725 - splt functions

--- src/components/Icon/buildStories.js
+++ src/components/Icon/buildStories.js
@@ -3,33 +3,15 @@
 // "If you are developing for Node.js then you most likely do not want this rule enabled."
 
 import fs from 'fs'
+import getCamelCase from '../../functions/getCamelCase.js'
+import sortArray from '../../functions/sortArray.js'
 import icons from '../../lib/icons.js'
 
-const sortArray = (arr) =>
-    arr.sort((a, b) => {
-        const fa = a.title.toLowerCase()
-        const fb = b.title.toLowerCase()
-
-        if (fa < fb) {
-            return -1
-        }
-        if (fa > fb) {
-            return 1
-        }
-        return 0
-    })
-
-const camel = (str) => {
-    return ` ${str}`
-        .toLowerCase()
-        .replace(/[^a-zA-Z0-9]+(.)/g, (match, chr) => chr.toUpperCase())
-}
-
-const stories = sortArray(icons)
+const stories = sortArray(icons, 'title')
     .map((item) => {
         return `
-export const ${camel(item.title)} = Template.bind({})
-${camel(item.title)}.args = { icon: '${item.type}' }
+export const ${getCamelCase(item.title)} = Template.bind({})
+${getCamelCase(item.title)}.args = { icon: '${item.type}' }
 `
     })
     .join(' ')
