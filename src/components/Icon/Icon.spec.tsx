1106c0d0b0b6dc0f8642ceb994224a0386089de3
Marcelo <glacial@gmail.com>
2021-12-17 08:53:56 -0500

    WSDEV-735 - add tests

--- src/components/Icon/Icon.spec.tsx
+++ src/components/Icon/Icon.spec.tsx
@@ -1,21 +1,20 @@
-import data from './IconData.json'
+import icons from '../../lib/icons'
 
 describe('Icon', () => {
     beforeEach(() => {
         cy.global() // running the global state test commands before each test
     })
 
-    const { classname, variations } = data
-
-    variations.map((variation) => {
-        const { title, icon } = variation
+    icons.map((variation) => {
+        const { title, type, path } = variation
         it(`${title}: Should render the icon`, () => {
             cy.visit(
                 `${Cypress.env(
                     'baseUrl'
-                )}/iframe.html?id=components-icon--${icon}`
+                )}/iframe.html?id=components-icon--${type.toLocaleLowerCase()}`
             )
-            cy.get(classname).should('exist')
+            cy.get('.c-icon').should('exist').and('not.be.empty')
+            cy.get('.c-icon g').should('exist').and('not.be.empty')
         })
     })
 })
