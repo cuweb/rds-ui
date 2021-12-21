fa82916b95c59ca2b2f7993249c29a45085e8fd3
Marcelo <glacial@gmail.com>
2021-12-21 09:59:12 -0500

    fix spaces

--- rollup.config.js
+++ rollup.config.js
@@ -8,29 +8,29 @@ import autoprefixer from 'autoprefixer'
 import packageJson from './package.json'
 
 export default {
-	input: 'src/index.ts',
-	output: [
-		{
-			file: packageJson.main,
-			format: 'cjs',
-			sourcemap: true,
-		},
-		{
-			file: packageJson.module,
-			format: 'esm',
-			sourcemap: true,
-		},
-	],
-	plugins: [
-		peerDepsExternal(),
-		resolve(),
-		commonjs(),
-		typescript({ useTsconfigDeclarationDir: true }),
-		scss({
-			watch: 'src',
-			output: 'dist/rds.css',
-			outputStyle: 'compressed',
-			processor: () => postcss([autoprefixer()]),
-		}),
-	],
+    input: 'src/index.ts',
+    output: [
+        {
+            file: packageJson.main,
+            format: 'cjs',
+            sourcemap: true,
+        },
+        {
+            file: packageJson.module,
+            format: 'esm',
+            sourcemap: true,
+        },
+    ],
+    plugins: [
+        peerDepsExternal(),
+        resolve(),
+        commonjs(),
+        typescript({ useTsconfigDeclarationDir: true }),
+        scss({
+            watch: 'src',
+            output: 'dist/rds.css',
+            outputStyle: 'compressed',
+            processor: () => postcss([autoprefixer()]),
+        }),
+    ],
 }
