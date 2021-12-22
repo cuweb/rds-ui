2657bc2cd6f4132c85732193a11701583373224a
Marcelo <glacial@gmail.com>
2021-12-22 08:56:06 -0500

    clean alert header

--- src/blocks/AlertHeader/AlertHeader.stories.tsx
+++ src/blocks/AlertHeader/AlertHeader.stories.tsx
@@ -14,22 +14,13 @@ const Template: Story = (args: any) => {
         return window.location.reload()
     }
 
-    return (
-        <>
-            <AlertHeader {...args} />
-            <br />
-            <br />
-            <br />
-            <button onClick={() => removeCookies()} className='c-buttoncta'>
-                clear cookies and show alert
-            </button>
-        </>
-    )
+    return <AlertHeader {...args} />
 }
 
 export const Base = Template.bind({})
 Base.args = {
     title: 'University closure, January 28, 2021!',
+    onClick: (e: never) => alert('click!'),
     description:
         'Due to inclement weather the university will be closed January 28, 2019. Please see the <a href="https://carleton.ca/ses/exam-schedule/">updated exam schedule</a>.',
 }
