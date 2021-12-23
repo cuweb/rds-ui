818e286a219a4dc7631d29fc67d804ea8d052739
HanqingZhou <72098908+HanqingZhou@users.noreply.github.com>
2021-12-23 11:34:23 -0500

    WSDEV-724 fix date

--- src/functions/convertDate.js
+++ src/functions/convertDate.js
@@ -1,5 +1,5 @@
 const convertData = (time) => {
-    const event = new Date(time)
+    const event = new Date(`${time}T00:00:00`)
     const options = { weekday: 'long', month: 'long', day: 'numeric' }
     return event.toLocaleDateString('en-US', options)
 }
