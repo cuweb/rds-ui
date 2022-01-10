34449ebd5d793653a7f1da6cc107588267723897
marceloglacial <glacial@gmail.com>
2022-01-10 11:19:40 -0500

    WSDEV-733 - add tests

--- src/blocks/CardGrid/CardGrid.spec.tsx
+++ src/blocks/CardGrid/CardGrid.spec.tsx
@@ -2,43 +2,33 @@ describe('Card Grid', () => {
     beforeEach(() => {
         cy.global()
     })
-    const block = {
-        url: 'blocks-alert-header--base',
-        className: '.b-alertheader',
-        title: 'h2',
-        content: 'p',
-        icon: '.c-icon',
-        closeButton: '.u-block > button',
-    }
 
-    it(`Should render container`, () => {
-        cy.visit(`${Cypress.env('baseUrl')}/iframe.html?id=${block.url}`)
-        cy.clearCookies()
-        cy.get(block.className).should('exist')
-        cy.get(`${block.className} .c-alert.c-alert--error`).should('exist')
-    })
+    const variations = [
+        { type: 'one', columns: '1' },
+        { type: 'two', columns: '2' },
+        { type: 'three', columns: '3' },
+        { type: 'four', columns: '4' },
+    ]
+    const title = 'h2'
+    const container = '.b-cardgrid.u-grid'
 
-    it(`Should render alert icon`, () => {
-        cy.get(
-            `${block.className} .c-alert.c-alert--error > ${block.icon}`
-        ).should('exist')
-    })
+    variations.map((variation) => {
+        const variationTitle = variation.type.toUpperCase()
+        it(`${variationTitle}: Should render container`, () => {
+            cy.visit(
+                `${Cypress.env('baseUrl')}/iframe.html?id=blocks-card-grid--${
+                    variation.type
+                }-column`
+            )
+            cy.get(`${container}`).should('exist')
+        })
 
-    it(`Should render title`, () => {
-        cy.get(`${block.className} ${block.title}`).should('exist')
-    })
+        it(`${variationTitle}: Should render ${variation.columns} column(s)`, () => {
+            cy.get(`.u-grid--${variation.columns}`).should('exist')
+        })
 
-    it(`Should render content`, () => {
-        cy.get(`${block.className} ${block.content}`).should('exist')
-    })
-
-    it(`Should render close button`, () => {
-        cy.get(block.closeButton).should('exist')
-        cy.get(`${block.closeButton} > ${block.icon}`).should('exist')
-    })
-
-    it(`Should render close alert on click`, () => {
-        cy.get(block.closeButton).click()
-        cy.get(block.className).should('not.exist')
+        it(`${variationTitle}: Should render title`, () => {
+            cy.get(title).should('exist')
+        })
     })
 })
