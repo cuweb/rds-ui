b8b4508fa93fb29430e9b2fb7e41799a93b13e98
HanqingZhou <72098908+HanqingZhou@users.noreply.github.com>
2022-01-11 17:39:55 -0500

    WSDEV-716 modification

--- src/components/Accordion/Accordion.tsx
+++ src/components/Accordion/Accordion.tsx
@@ -1,6 +1,4 @@
-import React, { ReactElement } from 'react'
-import BaseAccordion from './components/BaseAccordion'
-import SingleVariantAccordion from './components/SingleVariantAccordion'
+import React from 'react'
 
 export interface AccordionProps {
     type: 'base' | 'single'
@@ -13,18 +11,28 @@ export interface AccordionItemProps {
     content: string
 }
 
-export interface TypeProps {
-    [index: string]: ReactElement
-}
-
-const Accordion: React.FC<AccordionProps> = (props): JSX.Element => {
-    const { type } = props
-
-    const AccordionTypes: TypeProps = {
-        base: <BaseAccordion {...props} />,
-        single: <SingleVariantAccordion {...props} />,
-    }
-    return AccordionTypes[type || 'base']
+const Accordion: React.FC<AccordionProps> = ({
+    type, 
+    accordionData
+}): JSX.Element => {
+    const accordionClass = type === 'single' ? 'accordion__input--single' : ''
+    const accordionType = type === 'single' ? 'radio' : 'checkbox'
+    const accordionId = type === 'single' ? 's' : ''
+    return (
+        <>
+            {accordionData.map((accordion) => 
+                <div key={accordion.id} className="c-accordion">
+                    <input className={`accordion__input ${accordionClass}`} id={`accordion-id-${accordion.id}${accordionId}`}  name={`accordion-${accordionType}`} type="checkbox" />
+                    <label htmlFor={`accordion-id-${accordion.id}${accordionId}`} aria-controls={`accordion-aria-control-${accordion.id}${accordionId}`} aria-expanded="false" id={`accordion-labelledby-${accordion.id}${accordionId}`} role="heading" dangerouslySetInnerHTML={{ __html: accordion.title }} />
+                    <div id={`accordion-aria-control-${accordion.id}${accordionId}`} className="accordion__content" role="region" aria-labelledby={`accordion-labelledby-${accordion.id}${accordionId}`} aria-hidden="true">
+                        <div className="accordion__spacing">
+                            <p dangerouslySetInnerHTML={{ __html: accordion.content }} />
+                        </div>
+                    </div>
+                </div>
+            )}
+        </>
+    )
 }
 
 export default Accordion
