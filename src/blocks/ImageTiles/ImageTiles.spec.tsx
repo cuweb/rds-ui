db58eb3566caff63ca69d4066af5bd3c5d516f36
HanqingZhou <72098908+HanqingZhou@users.noreply.github.com>
2022-01-17 15:27:38 -0500

    WSDEV-807 add more test

--- src/blocks/ImageTiles/ImageTiles.spec.tsx
+++ src/blocks/ImageTiles/ImageTiles.spec.tsx
@@ -18,4 +18,76 @@ describe('ImageTiles', () => {
         cy.get(`.u-block--full`).should('exist')
     })
 
+    it(`Image Tiles: Should render b-imagetile`, () => {
+        cy.visit(
+            `${Cypress.env(
+                'baseUrl'
+            )}/iframe.html?id=blocks-image-tiles--base-image-tiles`
+        )
+        cy.get(`.b-imagetile`).should('exist')
+    })
+
+    it(`Image Tiles: Should render imagetile__container`, () => {
+        cy.visit(
+            `${Cypress.env(
+                'baseUrl'
+            )}/iframe.html?id=blocks-image-tiles--base-image-tiles`
+        )
+        cy.get(`.imagetile__container`).should('exist')
+    })
+
+    it(`Image Tiles: Should render anchor`, () => {
+        cy.visit(
+            `${Cypress.env(
+                'baseUrl'
+            )}/iframe.html?id=blocks-image-tiles--base-image-tiles`
+        )
+        cy.get(`a`).should('exist')
+    })
+
+    it(`Image Tiles: Should render imagetile__item`, () => {
+        cy.visit(
+            `${Cypress.env(
+                'baseUrl'
+            )}/iframe.html?id=blocks-image-tiles--base-image-tiles`
+        )
+        cy.get(`.imagetile__item`).should('exist')
+    })
+
+    it(`Image Tiles: Should render image`, () => {
+        cy.visit(
+            `${Cypress.env(
+                'baseUrl'
+            )}/iframe.html?id=blocks-image-tiles--base-image-tiles`
+        )
+        cy.get(`img`).should('exist')
+    })
+
+    it(`Image Tiles: Should render imagetile__content`, () => {
+        cy.visit(
+            `${Cypress.env(
+                'baseUrl'
+            )}/iframe.html?id=blocks-image-tiles--base-image-tiles`
+        )
+        cy.get(`.imagetile__content`).should('exist')
+    })
+
+    it(`Image Tiles: Should render header`, () => {
+        cy.visit(
+            `${Cypress.env(
+                'baseUrl'
+            )}/iframe.html?id=blocks-image-tiles--base-image-tiles`
+        )
+        cy.get(`h3`).should('exist')
+    })
+
+    it(`Image Tiles: Should render paragraph`, () => {
+        cy.visit(
+            `${Cypress.env(
+                'baseUrl'
+            )}/iframe.html?id=blocks-image-tiles--base-image-tiles`
+        )
+        cy.get(`p`).should('exist')
+    })
+
 })
