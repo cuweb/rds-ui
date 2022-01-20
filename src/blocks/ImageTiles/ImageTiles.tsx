45ac608918d842e90326782754be1681adb9bd86
HanqingZhou <72098908+HanqingZhou@users.noreply.github.com>
2022-01-20 12:15:52 -0500

    WSDEV-807 fix problem

--- src/blocks/ImageTiles/ImageTiles.tsx
+++ src/blocks/ImageTiles/ImageTiles.tsx
@@ -2,23 +2,16 @@ import React from 'react'
 import Ublock from '@src/components/Ublock/Ublock'
 
 export interface ImageTilesProps {
-    data: ImageTilesMainProps
+    data: ImageTilesDataProps[]
 }
 
-export interface ImageTilesMainProps {
-    image: HTMLImageElement
-    title: string
-    description: string
-    href: string
-    tilesData: TilesProps[]
-}
-
-export interface TilesProps {
+export interface ImageTilesDataProps {
     image: HTMLImageElement
     title: string
     description: string
     href: string
 }
+
 
 const ImageTiles: React.FC<ImageTilesProps> = ({
     data
@@ -28,16 +21,16 @@ const ImageTiles: React.FC<ImageTilesProps> = ({
         <Ublock full>
             <div className="b-imagetile">
                 <div className="imagetile__container">
-                    <a href={data.href} className="imagetile__item">
-                        <img src={data.image.src} alt={data.image.alt} />
+                    <a href={data[0].href} className="imagetile__item">
+                        <img src={data[0].image.src} alt={data[0].image.alt} />
                         <div className="imagetile__content">
-                            <h3>{data.title}</h3>
-                            <p>{data.description}</p>
+                            <h3>{data[0].title}</h3>
+                            <p>{data[0].description}</p>
                         </div>
                     </a>
                 </div>
                 <div className="imagetile__container">
-                    {data.tilesData.map((tile, index) => 
+                    {data.slice(1).map((tile, index) => 
                         <a key={index} href={tile.href} className="imagetile__item">
                             <img src={tile.image.src} alt={tile.image.alt} />
                             <div className="imagetile__content">
@@ -47,7 +40,6 @@ const ImageTiles: React.FC<ImageTilesProps> = ({
                         </a>
                     )}
                 </div>
-
             </div>
         </Ublock>
     )
