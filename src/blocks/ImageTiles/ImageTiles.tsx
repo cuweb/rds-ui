300603cce936402e2cfd9e96496aa28cea0a4122
marceloglacial <glacial@gmail.com>
2022-01-21 12:59:13 -0500

    WSDEV-807 - description option

--- src/blocks/ImageTiles/ImageTiles.tsx
+++ src/blocks/ImageTiles/ImageTiles.tsx
@@ -12,33 +12,33 @@ export interface ImageTilesDataProps {
     href: string
 }
 
-
-const ImageTiles: React.FC<ImageTilesProps> = ({
-    data
-}): JSX.Element => {
-
+const ImageTiles: React.FC<ImageTilesProps> = ({ data }): JSX.Element => {
     return (
         <Ublock full>
-            <div className="b-imagetile">
-                <div className="imagetile__container">
-                    <a href={data[0].href} className="imagetile__item">
+            <div className='b-imagetile'>
+                <div className='imagetile__container'>
+                    <a href={data[0].href} className='imagetile__item'>
                         <img src={data[0].image.src} alt={data[0].image.alt} />
-                        <div className="imagetile__content">
+                        <div className='imagetile__content'>
                             <h3>{data[0].title}</h3>
                             <p>{data[0].description}</p>
                         </div>
                     </a>
                 </div>
-                <div className="imagetile__container">
-                    {data.slice(1).map((tile, index) => 
-                        <a key={index} href={tile.href} className="imagetile__item">
+                <div className='imagetile__container'>
+                    {data.slice(1).map((tile, index) => (
+                        <a
+                            key={index}
+                            href={tile.href}
+                            className='imagetile__item'
+                        >
                             <img src={tile.image.src} alt={tile.image.alt} />
-                            <div className="imagetile__content">
+                            <div className='imagetile__content'>
                                 <h3>{tile.title}</h3>
-                                <p>{tile.description}</p>
+                                {tile.description && <p>{tile.description}</p>}
                             </div>
                         </a>
-                    )}
+                    ))}
                 </div>
             </div>
         </Ublock>
