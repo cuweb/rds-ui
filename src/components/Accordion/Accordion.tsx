bb8e094f7995064a6bed98343e20dc284ef28109
marceloglacial <glacial@gmail.com>
2022-01-25 11:32:33 -0500

    WSDEV-716 - fix eslint rule

--- src/components/Accordion/Accordion.tsx
+++ src/components/Accordion/Accordion.tsx
@@ -1,5 +1,3 @@
-/* eslint-disable jsx-a11y/role-supports-aria-props */
-/* eslint-disable jsx-a11y/label-has-associated-control */
 import React, { useState } from 'react'
 
 export interface AccordionProps {
@@ -13,79 +11,81 @@ export interface AccordionItemProps {
     content: string
 }
 
-const Accordion: React.FC<AccordionProps> = ({
-    type, 
-    data
-}): JSX.Element => {
+const Accordion: React.FC<AccordionProps> = ({ type, data }): JSX.Element => {
     const accordionClass = type === 'single' ? 'accordion__input--single' : ''
     const accordionType = type === 'single' ? 'radio' : 'checkbox'
     const accordionId = type === 'single' ? 's' : ''
     const [ariaState, setAriaState] = useState(true)
     const [checkedState, setCheckedState] = useState(
         new Array(data.length).fill(false)
-    );
-    const [divState, setDivState] = useState(
-        new Array(data.length).fill(false)
-    );
-    const toggleHandler=()=>{
+    )
+    const [divState, setDivState] = useState(new Array(data.length).fill(false))
+    const toggleHandler = () => {
         setAriaState(!ariaState)
     }
-    const handleOnChange = (position:number, accordionTypes: 'base' | 'single') => {
+    const handleOnChange = (
+        position: number,
+        accordionTypes: 'base' | 'single'
+    ) => {
         if (accordionTypes === 'base') {
             const updatedCheckedState = checkedState.map((item, index) =>
-            index === position ? !item : item
-          );
-          setCheckedState(updatedCheckedState);
+                index === position ? !item : item
+            )
+            setCheckedState(updatedCheckedState)
         } else {
             const updatedCheckedState = checkedState.map((item, index) =>
-            index === position ? !item : false
-          );
-          setCheckedState(updatedCheckedState);
-        const updatedDivState = divState.map((item, index) => {
-            if (index === position  && checkedState[index] === false) {
-              return true;
-            } 
-              return false;
-          });
-          setDivState(updatedDivState);
-        };
+                index === position ? !item : false
+            )
+            setCheckedState(updatedCheckedState)
+            const updatedDivState = divState.map((item, index) => {
+                if (index === position && checkedState[index] === false) {
+                    return true
+                }
+                return false
+            })
+            setDivState(updatedDivState)
+        }
     }
     return (
         <>
-            {data.map((accordion, index) => 
-                <div key={index} className="c-accordion">
-                    <input 
-                        className={`accordion__input ${accordionClass}`} 
-                        id={`accordion-id-${index}${accordionId}`}  
-                        name={`accordion-${accordionType}`} 
-                        type="checkbox" 
-                        aria-expanded={!ariaState} 
-                        onClick={toggleHandler} 
-                        checked={checkedState[index]} 
+            {data.map((accordion, index) => (
+                <div key={index} className='c-accordion'>
+                    <input
+                        className={`accordion__input ${accordionClass}`}
+                        id={`accordion-id-${index}${accordionId}`}
+                        name={`accordion-${accordionType}`}
+                        type='checkbox'
+                        aria-expanded={!ariaState}
+                        onClick={toggleHandler}
+                        checked={checkedState[index]}
                         onChange={() => handleOnChange(index, type)}
-                    /> 
-                    <label 
-                        htmlFor={`accordion-id-${index}${accordionId}`} 
-                        aria-controls={`accordion-aria-control-${index}${accordionId}`} 
-                        id={`accordion-labelledby-${index}${accordionId}`} 
-                        role="heading" 
-                        aria-level={1} 
-                        aria-hidden={ariaState} 
-                        dangerouslySetInnerHTML={{ __html: accordion.title }} 
-                    />                    
-                    <div id={`accordion-aria-control-${divState[index]} ${index}${accordionId}`} 
-                        className="accordion__content" 
-                        role="region" 
-                        aria-labelledby={`accordion-labelledby-${index}${accordionId}`} 
-                        aria-hidden={ariaState} 
+                    />
+                    <label
+                        htmlFor={`accordion-id-${index}${accordionId}`}
+                        aria-controls={`accordion-aria-control-${index}${accordionId}`}
+                        id={`accordion-labelledby-${index}${accordionId}`}
+                        role='heading'
+                        aria-level={1}
+                        aria-hidden={ariaState}
+                        dangerouslySetInnerHTML={{ __html: accordion.title }}
+                    />
+                    <div
+                        id={`accordion-aria-control-${divState[index]} ${index}${accordionId}`}
+                        className='accordion__content'
+                        role='region'
+                        aria-labelledby={`accordion-labelledby-${index}${accordionId}`}
+                        aria-hidden={ariaState}
                     >
-                        <div className="accordion__spacing">
-                            <p dangerouslySetInnerHTML={{ __html: accordion.content }} />
+                        <div className='accordion__spacing'>
+                            <p
+                                dangerouslySetInnerHTML={{
+                                    __html: accordion.content,
+                                }}
+                            />
                         </div>
-                    </div> 
-
+                    </div>
                 </div>
-            )}
+            ))}
         </>
     )
 }
