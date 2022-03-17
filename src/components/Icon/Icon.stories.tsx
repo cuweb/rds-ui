0e3001c24353a1f3945cdff74bd135e09a515b9f
marceloglacial <glacial@gmail.com>
2022-03-17 11:23:43 -0400

    WSDEV-860 - fixing icon lib

--- src/components/Icon/Icon.stories.tsx
+++ src/components/Icon/Icon.stories.tsx
@@ -1,613 +1,613 @@
+import Icon from './Icon'
 import React from 'react'
 import { Meta, Story } from '@storybook/react'
-import Icon from './Icon'
-
+    
 export default {
     component: Icon,
     title: 'Components/Icon',
 } as Meta
-
+    
 const Template: Story = (args: any) => <Icon {...args} size={42} />
 
 export const Accessibility = Template.bind({})
 Accessibility.args = { icon: 'accessibility' }
-
+ 
 export const Add = Template.bind({})
 Add.args = { icon: 'add' }
-
+ 
 export const Alert = Template.bind({})
 Alert.args = { icon: 'alert' }
-
+ 
 export const Apple = Template.bind({})
 Apple.args = { icon: 'apple' }
-
+ 
 export const Archive = Template.bind({})
 Archive.args = { icon: 'archive' }
-
+ 
 export const ArrowDown = Template.bind({})
 ArrowDown.args = { icon: 'arrow-down' }
-
+ 
 export const ArrowLeft = Template.bind({})
 ArrowLeft.args = { icon: 'arrow-left' }
-
+ 
 export const ArrowRight = Template.bind({})
 ArrowRight.args = { icon: 'arrow-right' }
-
+ 
 export const ArrowUp = Template.bind({})
 ArrowUp.args = { icon: 'arrow-up' }
-
+ 
 export const Backpack = Template.bind({})
 Backpack.args = { icon: 'backpack' }
-
+ 
 export const Beaker = Template.bind({})
 Beaker.args = { icon: 'beaker' }
-
+ 
 export const Bell = Template.bind({})
 Bell.args = { icon: 'bell' }
-
+ 
 export const Binoculars = Template.bind({})
 Binoculars.args = { icon: 'binoculars' }
-
+ 
 export const Blackboard = Template.bind({})
 Blackboard.args = { icon: 'blackboard' }
-
+ 
 export const Boardroom = Template.bind({})
 Boardroom.args = { icon: 'boardroom' }
-
+ 
 export const Book = Template.bind({})
 Book.args = { icon: 'book' }
-
+ 
 export const Bookmark = Template.bind({})
 Bookmark.args = { icon: 'bookmark' }
-
+ 
 export const Briefcase = Template.bind({})
 Briefcase.args = { icon: 'briefcase' }
-
+ 
 export const Broadcast = Template.bind({})
 Broadcast.args = { icon: 'broadcast' }
-
+ 
 export const Browser = Template.bind({})
 Browser.args = { icon: 'browser' }
-
+ 
 export const Bug = Template.bind({})
 Bug.args = { icon: 'bug' }
-
+ 
 export const Building = Template.bind({})
 Building.args = { icon: 'building' }
-
+ 
 export const BusinessCard = Template.bind({})
 BusinessCard.args = { icon: 'business-card' }
-
+ 
 export const Calendar = Template.bind({})
 Calendar.args = { icon: 'calendar' }
-
+ 
 export const Certification = Template.bind({})
 Certification.args = { icon: 'cert' }
-
+ 
 export const Check = Template.bind({})
 Check.args = { icon: 'check' }
-
+ 
 export const Checklist = Template.bind({})
 Checklist.args = { icon: 'checklist' }
-
+ 
 export const ChevronDown = Template.bind({})
 ChevronDown.args = { icon: 'chevron-down' }
-
+ 
 export const ChevronLeft = Template.bind({})
 ChevronLeft.args = { icon: 'chevron-left' }
-
+ 
 export const ChevronRight = Template.bind({})
 ChevronRight.args = { icon: 'chevron-right' }
-
+ 
 export const ChevronUp = Template.bind({})
 ChevronUp.args = { icon: 'chevron-up' }
-
+ 
 export const CircleSlash = Template.bind({})
 CircleSlash.args = { icon: 'circle-slash' }
-
+ 
 export const CircuitBoard = Template.bind({})
 CircuitBoard.args = { icon: 'circuit-board' }
-
+ 
 export const Clippy = Template.bind({})
 Clippy.args = { icon: 'clippy' }
-
+ 
 export const Clock = Template.bind({})
 Clock.args = { icon: 'clock' }
-
+ 
 export const CloudDownload = Template.bind({})
 CloudDownload.args = { icon: 'cloud-download' }
-
+ 
 export const CloudUpload = Template.bind({})
 CloudUpload.args = { icon: 'cloud-upload' }
-
+ 
 export const Code = Template.bind({})
 Code.args = { icon: 'code' }
-
+ 
 export const CodeSnippet = Template.bind({})
 CodeSnippet.args = { icon: 'code-snippet' }
-
+ 
 export const Cogs = Template.bind({})
 Cogs.args = { icon: 'cogs' }
-
+ 
 export const Comment = Template.bind({})
 Comment.args = { icon: 'comment' }
-
+ 
 export const CommentDiscussion = Template.bind({})
 CommentDiscussion.args = { icon: 'comment-discussion' }
-
+ 
 export const Compass = Template.bind({})
 Compass.args = { icon: 'compass' }
-
+ 
 export const CreditCard = Template.bind({})
 CreditCard.args = { icon: 'credit-card' }
-
+ 
 export const Cushield = Template.bind({})
 Cushield.args = { icon: 'cushield' }
-
+ 
 export const Dash = Template.bind({})
 Dash.args = { icon: 'dash' }
-
+ 
 export const Dashboard = Template.bind({})
 Dashboard.args = { icon: 'dashboard' }
-
+ 
 export const Database = Template.bind({})
 Database.args = { icon: 'database' }
-
+ 
 export const DesktopDownload = Template.bind({})
 DesktopDownload.args = { icon: 'desktop-download' }
-
+ 
 export const DeviceCameraVideo = Template.bind({})
 DeviceCameraVideo.args = { icon: 'device-camera-video' }
-
+ 
 export const DeviceCamera = Template.bind({})
 DeviceCamera.args = { icon: 'device-camera' }
-
+ 
 export const DeviceDesktop = Template.bind({})
 DeviceDesktop.args = { icon: 'device-desktop' }
-
+ 
 export const DeviceMobile = Template.bind({})
 DeviceMobile.args = { icon: 'device-mobile' }
-
+ 
 export const Diamond = Template.bind({})
 Diamond.args = { icon: 'diamond' }
-
+ 
 export const DiffAdded = Template.bind({})
 DiffAdded.args = { icon: 'diff-added' }
-
+ 
 export const Dining = Template.bind({})
 Dining.args = { icon: 'dining' }
-
+ 
+export const Doc = Template.bind({})
+Doc.args = { icon: 'doc' }
+ 
 export const Documents = Template.bind({})
 Documents.args = { icon: 'documents' }
-
+ 
 export const Dot = Template.bind({})
 Dot.args = { icon: 'dot' }
-
+ 
 export const Ellipsis = Template.bind({})
 Ellipsis.args = { icon: 'ellipsis' }
-
+ 
 export const Eye = Template.bind({})
 Eye.args = { icon: 'eye' }
-
+ 
 export const Facebook = Template.bind({})
 Facebook.args = { icon: 'facebook' }
-
+ 
 export const FileBinary = Template.bind({})
 FileBinary.args = { icon: 'file-binary' }
-
+ 
 export const FileCode = Template.bind({})
 FileCode.args = { icon: 'file-code' }
-
+ 
 export const FileDirectory = Template.bind({})
 FileDirectory.args = { icon: 'file-directory' }
-
+ 
 export const FileExcel = Template.bind({})
 FileExcel.args = { icon: 'file-excel' }
-
+ 
 export const FileMedia = Template.bind({})
 FileMedia.args = { icon: 'file-media' }
-
+ 
 export const FilePdf = Template.bind({})
 FilePdf.args = { icon: 'file-pdf' }
-
+ 
 export const FileSymlinkDirectory = Template.bind({})
 FileSymlinkDirectory.args = { icon: 'file-symlink-directory' }
-
+ 
 export const FileSymlinkFile = Template.bind({})
 FileSymlinkFile.args = { icon: 'file-symlink-file' }
-
+ 
 export const FileWord = Template.bind({})
 FileWord.args = { icon: 'file-word' }
-
+ 
 export const FileZip = Template.bind({})
 FileZip.args = { icon: 'file-zip' }
-
+ 
 export const Flame = Template.bind({})
 Flame.args = { icon: 'flame' }
-
+ 
 export const Fold = Template.bind({})
 Fold.args = { icon: 'fold' }
-
+ 
 export const Frame = Template.bind({})
 Frame.args = { icon: 'frame' }
-
+ 
 export const Gavel = Template.bind({})
 Gavel.args = { icon: 'gavel' }
-
+ 
 export const Gear = Template.bind({})
 Gear.args = { icon: 'gear' }
-
+ 
 export const Gift = Template.bind({})
 Gift.args = { icon: 'gift' }
-
+ 
 export const Github = Template.bind({})
 Github.args = { icon: 'github' }
-
+ 
 export const Globe = Template.bind({})
 Globe.args = { icon: 'globe' }
-
+ 
 export const Grabber = Template.bind({})
 Grabber.args = { icon: 'grabber' }
-
+ 
 export const Grad = Template.bind({})
 Grad.args = { icon: 'grad' }
-
+ 
 export const Graph = Template.bind({})
 Graph.args = { icon: 'graph' }
-
+ 
 export const Grid = Template.bind({})
 Grid.args = { icon: 'grid' }
-
+ 
 export const Growth = Template.bind({})
 Growth.args = { icon: 'growth' }
-
+ 
 export const Healthcare = Template.bind({})
 Healthcare.args = { icon: 'healthcare' }
-
+ 
 export const Heart = Template.bind({})
 Heart.args = { icon: 'heart' }
-
+ 
 export const HeartHands = Template.bind({})
 HeartHands.args = { icon: 'heart-hands' }
-
+ 
 export const History = Template.bind({})
 History.args = { icon: 'history' }
-
+ 
 export const Home = Template.bind({})
 Home.args = { icon: 'home' }
-
+ 
 export const Hr = Template.bind({})
 Hr.args = { icon: 'hr' }
-
+ 
 export const Inbox = Template.bind({})
 Inbox.args = { icon: 'inbox' }
-
+ 
 export const Info = Template.bind({})
 Info.args = { icon: 'info' }
-
+ 
 export const Instagram = Template.bind({})
 Instagram.args = { icon: 'instagram' }
-
+ 
 export const Issue = Template.bind({})
 Issue.args = { icon: 'issue' }
-
+ 
 export const IssueFixed = Template.bind({})
 IssueFixed.args = { icon: 'issue-fixed' }
-
+ 
 export const Italic = Template.bind({})
 Italic.args = { icon: 'italic' }
-
+ 
 export const Jersey = Template.bind({})
 Jersey.args = { icon: 'jersey' }
-
+ 
 export const KebabHor = Template.bind({})
 KebabHor.args = { icon: 'kebab-hor' }
-
+ 
 export const KebabVert = Template.bind({})
 KebabVert.args = { icon: 'kebab-vert' }
-
+ 
 export const Key = Template.bind({})
 Key.args = { icon: 'key' }
-
+ 
 export const Keyboard = Template.bind({})
 Keyboard.args = { icon: 'keyboard' }
-
+ 
 export const Law = Template.bind({})
 Law.args = { icon: 'law' }
-
+ 
 export const LightBulb = Template.bind({})
 LightBulb.args = { icon: 'light-bulb' }
-
+ 
 export const LineGraph = Template.bind({})
 LineGraph.args = { icon: 'line-graph' }
-
+ 
 export const Link = Template.bind({})
 Link.args = { icon: 'link' }
-
+ 
 export const LinkExternal = Template.bind({})
 LinkExternal.args = { icon: 'link-external' }
-
+ 
 export const Linkedin = Template.bind({})
 Linkedin.args = { icon: 'linkedin' }
-
+ 
 export const ListOrdered = Template.bind({})
 ListOrdered.args = { icon: 'list-ordered' }
-
+ 
 export const ListUnordered = Template.bind({})
 ListUnordered.args = { icon: 'list-unordered' }
-
+ 
 export const Location = Template.bind({})
 Location.args = { icon: 'location' }
-
+ 
 export const Lock = Template.bind({})
 Lock.args = { icon: 'lock' }
-
+ 
 export const Mail = Template.bind({})
 Mail.args = { icon: 'mail' }
-
+ 
 export const MailRead = Template.bind({})
 MailRead.args = { icon: 'mail-read' }
-
+ 
 export const Map = Template.bind({})
 Map.args = { icon: 'map' }
-
+ 
 export const MarkRavens = Template.bind({})
 MarkRavens.args = { icon: 'mark-ravens' }
-
+ 
 export const MarkRavensWhite = Template.bind({})
 MarkRavensWhite.args = { icon: 'mark-ravens-white' }
-
+ 
 export const Mark360 = Template.bind({})
 Mark360.args = { icon: 'mark-360' }
-
+ 
 export const MarkCushield = Template.bind({})
 MarkCushield.args = { icon: 'mark-cushield' }
-
+ 
 export const MarkGithub = Template.bind({})
 MarkGithub.args = { icon: 'mark-github' }
-
+ 
 export const Megaphone = Template.bind({})
 Megaphone.args = { icon: 'megaphone' }
-
+ 
 export const Mention = Template.bind({})
 Mention.args = { icon: 'mention' }
-
+ 
 export const Milestone = Template.bind({})
 Milestone.args = { icon: 'milestone' }
-
+ 
 export const Mirror = Template.bind({})
 Mirror.args = { icon: 'mirror' }
-
+ 
 export const Montain = Template.bind({})
 Montain.args = { icon: 'montain' }
-
+ 
 export const Mute = Template.bind({})
 Mute.args = { icon: 'mute' }
-
+ 
 export const Newspaper = Template.bind({})
 Newspaper.args = { icon: 'newspaper' }
-
+ 
 export const Note = Template.bind({})
 Note.args = { icon: 'note' }
-
+ 
 export const OrgChart = Template.bind({})
 OrgChart.args = { icon: 'org-chart' }
-
+ 
 export const Package = Template.bind({})
 Package.args = { icon: 'package' }
-
+ 
 export const Paintcan = Template.bind({})
 Paintcan.args = { icon: 'paintcan' }
-
+ 
 export const Parking = Template.bind({})
 Parking.args = { icon: 'parking' }
-
+ 
 export const Parliament = Template.bind({})
 Parliament.args = { icon: 'parliament' }
-
+ 
+export const Pdf = Template.bind({})
+Pdf.args = { icon: 'pdf' }
+ 
 export const Pencil = Template.bind({})
 Pencil.args = { icon: 'pencil' }
-
+ 
 export const Person = Template.bind({})
 Person.args = { icon: 'person' }
-
+ 
 export const Pin = Template.bind({})
 Pin.args = { icon: 'pin' }
-
+ 
 export const Pinterest = Template.bind({})
 Pinterest.args = { icon: 'pinterest' }
-
+ 
 export const Play = Template.bind({})
 Play.args = { icon: 'play' }
-
+ 
 export const PlayButton = Template.bind({})
 PlayButton.args = { icon: 'play-button' }
-
+ 
 export const Plug = Template.bind({})
 Plug.args = { icon: 'plug' }
-
+ 
 export const Plus = Template.bind({})
 Plus.args = { icon: 'plus' }
-
+ 
+export const Ppt = Template.bind({})
+Ppt.args = { icon: 'ppt' }
+ 
 export const Project = Template.bind({})
 Project.args = { icon: 'project' }
-
+ 
 export const Pulse = Template.bind({})
 Pulse.args = { icon: 'pulse' }
-
+ 
 export const Question = Template.bind({})
 Question.args = { icon: 'question' }
-
+ 
 export const Quote = Template.bind({})
 Quote.args = { icon: 'quote' }
-
+ 
 export const RadioTower = Template.bind({})
 RadioTower.args = { icon: 'radio-tower' }
-
+ 
 export const Reply = Template.bind({})
 Reply.args = { icon: 'reply' }
-
+ 
 export const Report = Template.bind({})
 Report.args = { icon: 'report' }
-
+ 
 export const Residence = Template.bind({})
 Residence.args = { icon: 'residence' }
-
+ 
 export const Ribbon = Template.bind({})
 Ribbon.args = { icon: 'ribbon' }
-
+ 
 export const RoadSign = Template.bind({})
 RoadSign.args = { icon: 'road-sign' }
-
+ 
 export const Rocket = Template.bind({})
 Rocket.args = { icon: 'rocket' }
-
+ 
 export const Rss = Template.bind({})
 Rss.args = { icon: 'rss' }
-
+ 
 export const Ruby = Template.bind({})
 Ruby.args = { icon: 'ruby' }
-
+ 
 export const ScreenFull = Template.bind({})
 ScreenFull.args = { icon: 'screen-full' }
-
+ 
 export const ScreenNormal = Template.bind({})
 ScreenNormal.args = { icon: 'screen-normal' }
-
+ 
 export const Search = Template.bind({})
 Search.args = { icon: 'search' }
-
+ 
 export const Secret = Template.bind({})
 Secret.args = { icon: 'secret' }
-
+ 
 export const Server = Template.bind({})
 Server.args = { icon: 'server' }
-
+ 
 export const Settings = Template.bind({})
 Settings.args = { icon: 'settings' }
-
+ 
 export const Shield = Template.bind({})
 Shield.args = { icon: 'shield' }
-
+ 
 export const SignIn = Template.bind({})
 SignIn.args = { icon: 'sign-in' }
-
+ 
 export const SignOut = Template.bind({})
 SignOut.args = { icon: 'sign-out' }
-
+ 
 export const Smiley = Template.bind({})
 Smiley.args = { icon: 'smiley' }
-
+ 
 export const Sos = Template.bind({})
 Sos.args = { icon: 'sos' }
-
+ 
 export const Square = Template.bind({})
 Square.args = { icon: 'square' }
-
+ 
 export const Squirrel = Template.bind({})
 Squirrel.args = { icon: 'squirrel' }
-
+ 
 export const Star = Template.bind({})
 Star.args = { icon: 'star' }
-
+ 
 export const Stop = Template.bind({})
 Stop.args = { icon: 'stop' }
-
+ 
 export const Sync = Template.bind({})
 Sync.args = { icon: 'sync' }
-
+ 
 export const Tag = Template.bind({})
 Tag.args = { icon: 'tag' }
-
+ 
 export const Tasklist = Template.bind({})
 Tasklist.args = { icon: 'tasklist' }
-
+ 
 export const Telescope = Template.bind({})
 Telescope.args = { icon: 'telescope' }
-
+ 
 export const Terminal = Template.bind({})
 Terminal.args = { icon: 'terminal' }
-
+ 
 export const TextSize = Template.bind({})
 TextSize.args = { icon: 'text-size' }
-
+ 
 export const Theatre = Template.bind({})
 Theatre.args = { icon: 'theatre' }
-
+ 
 export const Thumbsdown = Template.bind({})
 Thumbsdown.args = { icon: 'thumbsdown' }
-
+ 
 export const Thumbsup = Template.bind({})
 Thumbsup.args = { icon: 'thumbsup' }
-
+ 
 export const Tools = Template.bind({})
 Tools.args = { icon: 'tools' }
-
+ 
 export const Trashcan = Template.bind({})
 Trashcan.args = { icon: 'trashcan' }
-
+ 
 export const TriangleDown = Template.bind({})
 TriangleDown.args = { icon: 'triangle-down' }
-
+ 
 export const TriangleLeft = Template.bind({})
 TriangleLeft.args = { icon: 'triangle-left' }
-
+ 
 export const TriangleRight = Template.bind({})
 TriangleRight.args = { icon: 'triangle-right' }
-
+ 
 export const TriangleUp = Template.bind({})
 TriangleUp.args = { icon: 'triangle-up' }
-
+ 
 export const Twitter = Template.bind({})
 Twitter.args = { icon: 'twitter' }
-
+ 
 export const Unfold = Template.bind({})
 Unfold.args = { icon: 'unfold' }
-
+ 
 export const Unmute = Template.bind({})
 Unmute.args = { icon: 'unmute' }
-
+ 
 export const Unverified = Template.bind({})
 Unverified.args = { icon: 'unverified' }
-
+ 
 export const Verified = Template.bind({})
 Verified.args = { icon: 'verified' }
-
+ 
 export const Versions = Template.bind({})
 Versions.args = { icon: 'versions' }
-
+ 
 export const VideoPlayer = Template.bind({})
 VideoPlayer.args = { icon: 'video-player' }
-
+ 
 export const Warning = Template.bind({})
 Warning.args = { icon: 'warning' }
-
+ 
 export const Watch = Template.bind({})
 Watch.args = { icon: 'watch' }
-
+ 
 export const X = Template.bind({})
 X.args = { icon: 'X' }
-
+ 
+export const Xls = Template.bind({})
+Xls.args = { icon: 'xls' }
+ 
 export const Youtube = Template.bind({})
 Youtube.args = { icon: 'youtube' }
-
+ 
 export const Zap = Template.bind({})
 Zap.args = { icon: 'zap' }
+ 
+export const Zip = Template.bind({})
+Zip.args = { icon: 'zip' }
 
-export const Raven = Template.bind({})
-Raven.args = { icon: 'raven', size: '128', viewBox: '0 0 128 128' }
 
-export const PDF = Template.bind({})
-PDF.args = { icon: 'pdf' }
-
-export const XLS = Template.bind({})
-XLS.args = { icon: 'xls' }
-
-export const DOC = Template.bind({})
-DOC.args = { icon: 'doc' }
-
-export const PPT = Template.bind({})
-PPT.args = { icon: 'ppt' }
-
-export const ZIP = Template.bind({})
-ZIP.args = { icon: 'zip' }
+    
