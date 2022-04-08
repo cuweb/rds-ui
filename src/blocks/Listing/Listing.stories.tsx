f0e7e9c6371e0b7a9e50acb929d48061222da296
marceloglacial <glacial@gmail.com>
2022-04-08 09:11:15 -0400

    WSDEV-804 - add dark mode to listing

--- src/blocks/Listing/Listing.stories.tsx
+++ src/blocks/Listing/Listing.stories.tsx
@@ -153,22 +153,22 @@ export const WithIcon = () => {
                 {
                     src: '#',
                     title: 'White-necked Raven',
-                    icon: 'mark-ravens',
+                    icon: 'github',
                 },
                 {
                     src: '#',
                     title: 'Common raven',
-                    icon: 'mark-ravens',
+                    icon: 'twitter',
                 },
                 {
                     src: '#',
                     title: 'Australian raven',
-                    icon: 'mark-ravens',
+                    icon: 'facebook',
                 },
                 {
                     src: '#',
                     title: 'Thick-billed raven',
-                    icon: 'mark-ravens',
+                    icon: 'linkedin',
                 },
             ]}
         />
