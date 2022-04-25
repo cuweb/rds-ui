eb65f64cdc7a330c781fa80f0d39c800446fc660
marceloglacial <glacial@gmail.com>
2022-04-25 08:30:53 -0400

    fix table styles

--- src/layouts/Layout/Layout.stories.tsx
+++ src/layouts/Layout/Layout.stories.tsx
@@ -20,6 +20,19 @@ export const Single = (args: any) => (
             section blocks can be used within the {`<main>`} section, including
             all fixed and full-width blocks.
         </p>
+        <table>
+            <thead>
+                <tr>
+                    <th colSpan={2}>The table header</th>
+                </tr>
+            </thead>
+            <tbody>
+                <tr>
+                    <td>The table body</td>
+                    <td>with two columns</td>
+                </tr>
+            </tbody>
+        </table>
     </Layout>
 )
 
