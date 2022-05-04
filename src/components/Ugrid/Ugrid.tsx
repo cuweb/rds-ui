0a51660b3eaef1618d996db26c2abec6f199b7e7
marceloglacial <glacial@gmail.com>
2022-05-04 12:12:27 -0400

    listings: fix width / grid: add more options / panel: max-width fix

--- src/components/Ugrid/Ugrid.tsx
+++ src/components/Ugrid/Ugrid.tsx
@@ -2,16 +2,25 @@ import React from 'react'
 
 export interface UgridProps {
     className?: string | undefined
-    columns?: 1 | 2 | 3 | 4 | undefined
+    columns?: 1 | 2 | 3 | 4
+    gap?: 1 | 2 | 3 | 4
+    isLayout?: boolean
 }
 
 const Ugrid: React.FC<UgridProps> = ({
     className = '',
     columns = 3,
     children,
+    gap = 2,
+    isLayout,
 }): JSX.Element => {
+    const layoutClassName = isLayout ? 'u-grid--layout' : ''
+    const columnsClassName = isLayout ? '' : `u-grid--${columns}`
+
     return (
-        <div className={`${className} u-grid--s1 u-grid u-grid--${columns}`}>
+        <div
+            className={`${className} u-grid--s1 u-grid u-grid--gap-${gap} ${columnsClassName} ${layoutClassName}`}
+        >
             {children}
         </div>
     )
